5c16e2b43928571ce5f9945c1586aa224ae6de94
johncarco <126143412+johncarco@users.noreply.github.com>
2023-03-04 12:49:24 +0800

    yes

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,5 @@
 #include <cstring>
 #include <iostream>
-
 using namespace std;
 
 int main(){
@@ -8,25 +7,25 @@ int main(){
     int Premium=250, Box=100, Sideline=50, GenAd=25, Total=0, Amount1, Amount2, Amount3, Amount4, TotalTix, TotalPremium, TotalBox, TotalSideline, TotalGenAd;
 
     cout<<"Ticket Prices"<<endl;
-    cout<<"\nPremium: "<<Premium<<endl;
+    cout<<"\nPremium: $"<<Premium<<endl;
     cout<<"Amount: ";
     cin>> Amount1;
 
     TotalPremium=Amount1*Premium;
 
-    cout<<"Box: "<<Box<<endl;
+    cout<<"Box: $"<<Box<<endl;
     cout<<"Amount: ";
     cin>> Amount2;
 
     TotalBox=Amount2*Box;
 
-    cout<<"Sideline: "<<Sideline<<endl;
+    cout<<"Sideline: $"<<Sideline<<endl;
     cout<<"Amount: ";
     cin>> Amount3;
 
     TotalSideline=Amount3*Sideline;
 
-    cout<<"General Ad: "<<GenAd<<endl;
+    cout<<"General Ad: $"<<GenAd<<endl;
     cout<<"Amount: ";
     cin>> Amount4;
 
@@ -36,16 +35,15 @@ int main(){
 
     Total=TotalPremium+TotalBox+TotalSideline+TotalGenAd;
 
-    cout<<"\nTotal Premium Tickets Availed: "<<TotalPremium<<endl;
-    cout<<"\nTotal Box Tickets Availed: "<<TotalBox<<endl;
-    cout<<"\nTotal Sideline Tickets Availed: "<<TotalSideline<<endl;
-    cout<<"\nTotal General Ad Tickets Availed: "<<TotalGenAd<<endl;
-    cout<<"\nTotal Sales: "<<Total<<endl;
-    cout<<"\nTotal Tickets Sold: "<<TotalTix<<endl;
+    cout<<"\nTotal Premium Tickets Sold: $ "<<TotalPremium<<endl;
+    cout<<"\nTotal Box Tickets Sold: $"<<TotalBox<<endl;
+    cout<<"\nTotal Sideline Tickets: $"<<TotalSideline<<endl;
+    cout<<"\nTotal General Ad Tickets: $"<<TotalGenAd<<endl;
+    cout<<"\nTotal Ticket Sales: $"<<Total<<endl;
+    cout<<"\nOverall Tickets Purchased: "<<TotalTix<<" Tickets"<<endl;
 
     return 0;
 
-
 }
 
-var test = "yes"
+    var test = "yes"
