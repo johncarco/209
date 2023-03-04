3ee906acabdd4b294422d98c9ee4c665c7e888ec
johncarco <126143412+johncarco@users.noreply.github.com>
2023-03-04 11:42:48 +0800

    yes

---
+++ 2.cpp
@@ -0,0 +1,51 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int Premium=250, Box=100, Sideline=50, GenAd=25, Total=0, Amount1, Amount2, Amount3, Amount4, TotalTix, TotalPremium, TotalBox, TotalSideline, TotalGenAd;
+
+    cout<<"Ticket Prices"<<endl;
+    cout<<"\nPremium: "<<Premium<<endl;
+    cout<<"Amount: ";
+    cin>> Amount1;
+
+    TotalPremium=Amount1*Premium;
+
+    cout<<"Box: "<<Box<<endl;
+    cout<<"Amount: ";
+    cin>> Amount2;
+
+    TotalBox=Amount2*Box;
+
+    cout<<"Sideline: "<<Sideline<<endl;
+    cout<<"Amount: ";
+    cin>> Amount3;
+
+    TotalSideline=Amount3*Sideline;
+
+    cout<<"General Ad: "<<GenAd<<endl;
+    cout<<"Amount: ";
+    cin>> Amount4;
+
+    TotalGenAd=Amount4*GenAd;
+
+    TotalTix=Amount1+Amount2+Amount3+Amount4;
+
+    Total=TotalPremium+TotalBox+TotalSideline+TotalGenAd;
+
+    cout<<"\nTotal Premium Tickets Availed: "<<TotalPremium<<endl;
+    cout<<"\nTotal Box Tickets Availed: "<<TotalBox<<endl;
+    cout<<"\nTotal Sideline Tickets Availed: "<<TotalSideline<<endl;
+    cout<<"\nTotal General Ad Tickets Availed: "<<TotalGenAd<<endl;
+    cout<<"\nTotal Sales: "<<Total<<endl;
+    cout<<"\nTotal Tickets Sold: "<<TotalTix<<endl;
+
+    return 0;
+
+
+}
+
+var test = "yes"
